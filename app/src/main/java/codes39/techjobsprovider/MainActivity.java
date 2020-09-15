39fd06902aede1bb02cfdb49b3994c1ac977a2f7
Kimkims1 <kimanthikevin76@gmail.com>
2020-09-16 02:00:37 +0300

    tabs

--- app/src/main/java/codes39/techjobsprovider/MainActivity.java
+++ app/src/main/java/codes39/techjobsprovider/MainActivity.java
@@ -1,14 +1,95 @@
 package codes39.techjobsprovider;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
 
+import com.google.android.material.badge.BadgeDrawable;
+import com.google.android.material.tabs.TabLayout;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
+
+    private Toolbar toolbar;
+    private ViewPager viewPager;
+    private TabLayout tabLayout;
+
+   private MyJobsFragment myJobsFragment;
+   private AppliedFragment appliedFragment;
+   private OthersFragment othersFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        viewPager = findViewById(R.id.view_pager);
+        tabLayout = findViewById(R.id.tab_layout);
+
+        myJobsFragment = new MyJobsFragment();
+        appliedFragment = new AppliedFragment();
+        othersFragment = new OthersFragment();
+
+        tabLayout.setupWithViewPager(viewPager);
+
+        ViewPagerAdapter viewPagerAdapter = new ViewPagerAdapter(getSupportFragmentManager(), 0);
+
+        viewPagerAdapter.addFragment(myJobsFragment, "My feed");
+        viewPagerAdapter.addFragment(appliedFragment, "Applied");
+        viewPagerAdapter.addFragment(othersFragment, "Others");
+
+        viewPager.setAdapter(viewPagerAdapter);
+
+        tabLayout.getTabAt(0).setIcon(R.drawable.ic_my_feed);
+        tabLayout.getTabAt(1).setIcon(R.drawable.ic_applied);
+        tabLayout.getTabAt(2).setIcon(R.drawable.ic_others);
+
+        BadgeDrawable badgeDrawable = tabLayout.getTabAt(0).getOrCreateBadge();
+        badgeDrawable.setVisible(true);
+    }
+
+    private class ViewPagerAdapter extends FragmentPagerAdapter {
+
+        private List<Fragment> fragments = new ArrayList<>();
+        private List<String> fragmentTitle = new ArrayList<>();
+
+        public ViewPagerAdapter(@NonNull FragmentManager fm, int behavior) {
+            super(fm, behavior);
+        }
+
+        public void addFragment(Fragment fragment, String title) {
+
+            fragments.add(fragment);
+            fragmentTitle.add(title);
+        }
+
+        @Nullable
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return fragmentTitle.get(position);
+        }
+
+        @NonNull
+        @Override
+        public Fragment getItem(int position) {
+            return fragments.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            return fragments.size();
+        }
     }
 }
